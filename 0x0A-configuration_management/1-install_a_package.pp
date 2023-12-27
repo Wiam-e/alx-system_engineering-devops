9fe8d9b774d906b02300525a71f8059d1398ca78
Wiam-e <wiam.errabih@gmail.com>
2023-12-27 13:57:26 -0800

    Install flask again

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,6 @@
-# Install flask
-exec { 'flask':
-  command => '/usr/bin/apt-get -y install flask -v 2.1.0',
+#!/usr/bin/pup
+# Install flask 2.1.0
+package {'flask':
+ensure => '2.1.0'
+provider => 'pip3'
 }
